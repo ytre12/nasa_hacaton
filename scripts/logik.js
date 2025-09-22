7dcde99ae03906eea90c556b06c599e8b29c6230
ytre12 <obio12yt@gmail.com>
2025-09-22 21:16:04 +0300

    Add files via upload

--- scripts/logik.js
+++ scripts/logik.js
@@ -6,11 +6,15 @@ const cardOneElement = document.getElementById("card-1");
 const cardTwoElement = document.getElementById("card-2");
 const cardThreeElement = document.getElementById("card-3");
 
+const leftPerson = document.querySelector("#person-left-id");
+const raghtPerson = document.querySelector("#person-right-id");
+
 const leftPersonName = document.querySelector("#person-left");
 const raghtPersonName = document.querySelector("#person-right");
 
 const restartWindow = document.querySelector(".lose-window");
 const restartBtn = document.getElementById("btn-restart");
+
 
 cardOneElement.hidden = true;
 cardTwoElement.hidden = true;
@@ -28,13 +32,34 @@ const SeeActivPerson = () => {
     if (dialogArr[dialogCounter][0] === "left") {
         leftPersonName.className = "person-name-activ";
         raghtPersonName.className = "person-name";
+        
+        leftPerson.className = "person-activ";
+        raghtPerson.className = "person";
     }else if (dialogArr[dialogCounter][0] === "right") {
         leftPersonName.className = "person-name";
         raghtPersonName.className = "person-name-activ";
+
+        leftPerson.className = "person";
+        raghtPerson.className = "person-activ";
     }else {
         leftPersonName.className = "person-name";
         raghtPersonName.className = "person-name";    
+    
+        leftPerson.className = "person";
+        raghtPerson.className = "person";
     }
+}
+
+const ChangeImgItems = () => {
+    cardOneElement.firstElementChild.remove();
+    cardTwoElement.firstElementChild.remove();
+    cardThreeElement.firstElementChild.remove();
+
+    cardOneElement.insertAdjacentHTML("afterbegin", itemSelectorImg[itemsSelectorStage][0]);
+    cardTwoElement.insertAdjacentHTML("afterbegin", itemSelectorImg[itemsSelectorStage][1]);
+    cardThreeElement.insertAdjacentHTML("afterbegin", itemSelectorImg[itemsSelectorStage][2]);
+
+    itemsSelectorStage++;
 }
 
 const dialogArr = [ 
@@ -44,7 +69,11 @@ const dialogArr = [
                     ["", "Виберіть селектор:"],
                     ["left", "Діалог 5"],
                     ["right", "Діалог 6"],
-                    ["left", "Діалог 7"],
+                    ["left", `qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+                        qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq`],
+                    ["left", "Діалог 9"],
+                    ["left", "Діалог 10"],
+                    ["", "Виберіть селектор:"],
                     ["", "Далі буде)"],];
 
 const TrueDialog = [
@@ -53,8 +82,10 @@ const TrueDialog = [
 
 const LoseLogic = [
     [false, false, true],
-
+    [true, true, false],
 ]
+
+let losVar = 0;
 
 const loseImg = [
     [`<img src="./lose-img/lose-img.png" alt="" style="width: 350px; margin-top: 70px">`,
@@ -62,19 +93,29 @@ const loseImg = [
     `<img src="./lose-img/lose-img.png" alt="" style="width: 350px; margin-top: 70px">`]
     ];
 
+const itemSelectorImg = [   
+                            [
+                            `<img src="./lose-img/lose-img.png" alt="card-1" width="100px">`, 
+                            `<img src="./lose-img/lose-img.png" alt="card-2" width="100px">`,
+                            `<img src="./lose-img/lose-img.png" alt="card-3" width="100px">`
+                            ],
+
+                        ];
+let itemsSelectorStage = 0;
+
 let dialogCounter = 0;
 
 let dialogWork = true;
 
 const HideSelector = () => {
-    if (dialogCounter == 4) {
+    if (dialogCounter == 4 || dialogCounter == 10) {
         ChangeSelector();
         dialogWork=false;
     }  
 }
 
 const DialogCounterActiv = () => {
-    if (dialogCounter >= 8) {
+    if (dialogCounter >= 11) {
         dialogWork = false;
     }
 }
@@ -92,33 +133,39 @@ dialogWindowElement.onclick = function() {
 cardOneElement.onclick = function() {
     dialogWindowElement.textContent = "Був обраний селектор 1";
     ChangeSelector();
-    if (LoseLogic[0][0]) {
+    if (LoseLogic[losVar][0]) {
         SeeLoseWindow(0)
     }else{
         dialogWork = true;
         dialogWindowElement.textContent = TrueDialog[0];
+        ChangeImgItems();
+        losVar++;
     }
 }
 
 cardTwoElement.onclick = function() {
     dialogWindowElement.textContent = "Був обраний селектор 2";
     ChangeSelector();
-    if (LoseLogic[0][1]) {
+    if (LoseLogic[losVar][1]) {
         SeeLoseWindow(1)
     }else{
         dialogWork = true;
         dialogWindowElement.textContent = TrueDialog[0];
+        ChangeImgItems();
+        losVar++;
     }
 }
 
 cardThreeElement.onclick = function() {
     dialogWindowElement.textContent = "Був обраний селектор 3";
     ChangeSelector();
-    if (LoseLogic[0][2]) {
+    if (LoseLogic[losVar][2]) {
         SeeLoseWindow(2)
     }else{
         dialogWork = true;
         dialogWindowElement.textContent = TrueDialog[0];
+        ChangeImgItems();
+        losVar++;
     }
 }
 
@@ -137,6 +184,3 @@ const SeeLoseWindow = (numberImg) => {
     restartWindow.insertAdjacentHTML("afterbegin", loseImg[0][numberImg])
     restartWindow.hidden = false;
 }
-
-
-
